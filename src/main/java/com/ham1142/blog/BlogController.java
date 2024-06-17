ee614dca66b5dc0f89b424e875f1ce5ac2b0d19b
JBEDU29 <5514cyj@gmail.com>
2024-06-17 11:45:52 +0900

    spring controller, request, model 연습 추가

---
+++ src/main/java/com/ham1142/blog/BlogController.java
@@ -0,0 +1,46 @@
+package com.ham1142.blog;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BlogController {
+
+	@RequestMapping(value = "/home")
+	public String home() {
+		return "home";
+	}
+	@RequestMapping(value = "/user/test")
+	public String test() {
+		return "sample1";
+	}
+	
+	@RequestMapping(value = "/member")
+	public String member() {
+		return "user/member"; // 특정 폴더 내의 jsp 파일 실행
+	}
+	@RequestMapping(value = "/user/test2")
+	public String test2() {
+		return "sample2"; // 특정 폴더 내의 jsp 파일 실행
+	}
+	@RequestMapping(value = "login")
+	public String login() {
+		return "login";
+	}
+	@RequestMapping(value = "loginOk")
+	public String loginOk(HttpServletRequest request, Model model) {
+		String mid = request.getParameter("memberid"); // tiger
+		String mpw = request.getParameter("memberpw"); // 12345
+		
+		model.addAttribute("loginid", mid);
+		model.addAttribute("loginpw", mpw);
+		model.addAttribute("user", "홍길동");
+		
+		
+		return "loginOk";
+	}
+	
+}
